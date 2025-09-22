d8b3527304e6d139d51ef8525cfdefef5eb9d041
Shouib Ahamed <shouibahamed2005@gmail.com>
2025-09-22 21:56:24 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-});
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
